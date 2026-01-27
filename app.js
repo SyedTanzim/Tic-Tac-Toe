48f6dc8743a54a85990aa0b498e96c7d655cf875
tanzim <syedtanzimwajih@gmail.com>
2026-01-27 22:37:15 +0530

    fix: correct vertical win condition logic

--- app.js
+++ app.js
@@ -61,15 +61,15 @@ const gameController = (function () {
         for (let i = 0; i < board.length; i++) {
             for (let j = 1; j < board.length - 1; j++) {
                 if (board[i][j - 1] != null && board[i][j - 1] === board[i][j] && board[i][j] === board[i][j + 1]) {
-                    return true
+                    return true;
                 }
             }
         }
 
-        for (let i = 1; i < board.length - 1; i++) {
-            for (let j = 0; j < board.length; j++) {
-                if (board[j][i - 1] != null && board[j][i - 1] === board[j][i] && board[j][i] === board[j][i + 1]) {
-                    return true
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 1; j < board.length - 1; j++) {
+                if (board[j - 1][i] != null && board[j - 1][i] === board[j][i] && board[j][i] === board[j + 1][i]) {
+                    return true;
                 }
             }
         }
